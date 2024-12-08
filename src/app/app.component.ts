9f643b37056e9d58edaf751d0ccf2c1e2a493163
yobett <yobett@hxr.com>
2024-12-08 19:34:01 +0800

    echarts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ECharts } from 'echarts';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +9,12 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  @ViewChild('chart') chartDiv: ElementRef|undefined;
+  chart: ECharts|undefined;
+
   title = 'ops-ng';
+
+  chartWidth = '100%';
+  chartHeight = 400;
+
 }
