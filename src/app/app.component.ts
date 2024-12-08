a2499945b273e56d1908c6eecc74e87fcc8f52fa
yobett <yobett@hxr.com>
2024-12-08 20:32:14 +0800

    kline chart

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { AfterContentInit, AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ECharts } from 'echarts';
+import * as echarts from 'echarts';
+import rawData from './d.json'
 
 @Component({
   selector: 'app-root',
@@ -8,13 +10,289 @@ import { ECharts } from 'echarts';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  @ViewChild('chart') chartDiv: ElementRef|undefined;
-  chart: ECharts|undefined;
+export class AppComponent implements OnInit, AfterViewInit {
+  @ViewChild('chart') chartDiv: ElementRef | undefined;
+  chart: ECharts | undefined;
 
   title = 'ops-ng';
 
   chartWidth = '100%';
   chartHeight = 400;
 
+
+  async ngOnInit() {
+  }
+
+  async ngAfterViewInit() {
+
+    type EChartsOption = echarts.EChartsOption;
+
+    const holder: HTMLDivElement = this.chartDiv!.nativeElement as HTMLDivElement;
+    this.chart = echarts.init(holder, null,
+      {
+        renderer: 'svg',
+        locale: 'ZH'
+      });
+    var myChart = this.chart;
+    var option: EChartsOption;
+
+    const upColor = '#00da3c';
+    const downColor = '#ec0000';
+
+    function splitData(rawData: number[][]) {
+      let categoryData = [];
+      let values = [];
+      let volumes = [];
+      for (let i = 0; i < rawData.length; i++) {
+        categoryData.push(rawData[i].splice(0, 1)[0]);
+        values.push(rawData[i]);
+        volumes.push([i, rawData[i][4], rawData[i][0] > rawData[i][1] ? 1 : -1]);
+      }
+
+      return {
+        categoryData: categoryData,
+        values: values,
+        volumes: volumes
+      };
+    }
+
+    function calculateMA(dayCount: number, data: { values: number[][] }) {
+      var result = [];
+      for (var i = 0, len = data.values.length; i < len; i++) {
+        if (i < dayCount) {
+          result.push('-');
+          continue;
+        }
+        var sum = 0;
+        for (var j = 0; j < dayCount; j++) {
+          sum += data.values[i - j][1];
+        }
+        result.push(+(sum / dayCount).toFixed(3));
+      }
+      return result;
+    }
+
+    // const res = await fetch(ROOT_PATH + '/data/asset/data/stock-DJI.json');
+    // const rawData = rawData;
+
+    var data = splitData(rawData as any[]);
+
+    option = {
+      animation: false,
+      legend: {
+        bottom: 10,
+        left: 'center',
+        data: ['Dow-Jones index', 'MA5', 'MA10', 'MA20', 'MA30']
+      },
+      tooltip: {
+        trigger: 'axis',
+        axisPointer: {
+          type: 'cross'
+        },
+        borderWidth: 1,
+        borderColor: '#ccc',
+        padding: 10,
+        textStyle: {
+          color: '#000'
+        },
+        position: function (pos, params, el, elRect, size) {
+          const obj: Record<string, number> = {
+            top: 10
+          };
+          obj[['left', 'right'][+(pos[0] < size.viewSize[0] / 2)]] = 30;
+          return obj;
+        }
+        // extraCssText: 'width: 170px'
+      },
+      axisPointer: {
+        link: [
+          {
+            xAxisIndex: 'all'
+          }
+        ],
+        label: {
+          backgroundColor: '#777'
+        }
+      },
+      toolbox: {
+        feature: {
+          dataZoom: {
+            yAxisIndex: false
+          },
+          brush: {
+            type: ['lineX', 'clear']
+          }
+        }
+      },
+      brush: {
+        xAxisIndex: 'all',
+        brushLink: 'all',
+        outOfBrush: {
+          colorAlpha: 0.1
+        }
+      },
+      visualMap: {
+        show: false,
+        seriesIndex: 5,
+        dimension: 2,
+        pieces: [
+          {
+            value: 1,
+            color: downColor
+          },
+          {
+            value: -1,
+            color: upColor
+          }
+        ]
+      },
+      grid: [
+        {
+          left: '10%',
+          right: '8%',
+          height: '50%'
+        },
+        {
+          left: '10%',
+          right: '8%',
+          top: '63%',
+          height: '16%'
+        }
+      ],
+      xAxis: [
+        {
+          type: 'category',
+          data: data.categoryData,
+          boundaryGap: false,
+          axisLine: { onZero: false },
+          splitLine: { show: false },
+          min: 'dataMin',
+          max: 'dataMax',
+          axisPointer: {
+            z: 100
+          }
+        },
+        {
+          type: 'category',
+          gridIndex: 1,
+          data: data.categoryData,
+          boundaryGap: false,
+          axisLine: { onZero: false },
+          axisTick: { show: false },
+          splitLine: { show: false },
+          axisLabel: { show: false },
+          min: 'dataMin',
+          max: 'dataMax'
+        }
+      ],
+      yAxis: [
+        {
+          scale: true,
+          splitArea: {
+            show: true
+          }
+        },
+        {
+          scale: true,
+          gridIndex: 1,
+          splitNumber: 2,
+          axisLabel: { show: false },
+          axisLine: { show: false },
+          axisTick: { show: false },
+          splitLine: { show: false }
+        }
+      ],
+      dataZoom: [
+        {
+          type: 'inside',
+          xAxisIndex: [0, 1],
+          start: 98,
+          end: 100
+        },
+        {
+          show: true,
+          xAxisIndex: [0, 1],
+          type: 'slider',
+          top: '85%',
+          start: 98,
+          end: 100
+        }
+      ],
+      series: [
+        {
+          name: 'Dow-Jones index',
+          type: 'candlestick',
+          data: data.values,
+          itemStyle: {
+            color: upColor,
+            color0: downColor,
+            borderColor: undefined,
+            borderColor0: undefined
+          }
+        },
+        {
+          name: 'MA5',
+          type: 'line',
+          data: calculateMA(5, data),
+          smooth: true,
+          lineStyle: {
+            opacity: 0.5
+          }
+        },
+        {
+          name: 'MA10',
+          type: 'line',
+          data: calculateMA(10, data),
+          smooth: true,
+          lineStyle: {
+            opacity: 0.5
+          }
+        },
+        {
+          name: 'MA20',
+          type: 'line',
+          data: calculateMA(20, data),
+          smooth: true,
+          lineStyle: {
+            opacity: 0.5
+          }
+        },
+        {
+          name: 'MA30',
+          type: 'line',
+          data: calculateMA(30, data),
+          smooth: true,
+          lineStyle: {
+            opacity: 0.5
+          }
+        },
+        {
+          name: 'Volume',
+          type: 'bar',
+          xAxisIndex: 1,
+          yAxisIndex: 1,
+          data: data.volumes
+        }
+      ]
+    };
+
+    myChart.setOption(
+      option,
+      true
+    );
+
+    myChart.dispatchAction({
+      type: 'brush',
+      areas: [
+        {
+          brushType: 'lineX',
+          coordRange: ['2016-06-02', '2016-06-20'],
+          xAxisIndex: 0
+        }
+      ]
+    });
+
+    myChart.setOption(option);
+
+  }
 }
